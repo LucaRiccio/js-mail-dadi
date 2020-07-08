13feef33aa12627976ca0a7a2584c3092158cde9
LucaRiccio <lucariccio888@gmail.com>
2020-07-08 16:45:02 +0200

    Esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
-var mailAutorizzate = ["luca.riccio@gmail.com", "riccio.luca@gmail.com"]
 var mail = prompt("Inserici la tua Email")
+var mailAutorizzate = ["luca.riccio@gmail.com", "riccio.luca@gmail.com"]
+var autorizzata = false;
+
+for (var i = 0; i<mailAutorizzate.length; i++){
+  if (mail == mailAutorizzate[i]){
+  autorizzata = true
+  }
+}
+
+if (autorizzata == true){
+  alert("Mail autorizzata");
+}
+else {
+  alert("Mail non autorizzata");
+}
