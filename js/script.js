63f0da6858e8963c818d48d2917bd39357db2d6b
LucaRiccio <lucariccio888@gmail.com>
2020-07-09 14:03:14 +0200

    Esercizio Finito (aggiunto gioco dadi)

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var mail = prompt("Inserici la tua Email")
+var mail = prompt("Inserici la tua Email per giocare!")
 var mailAutorizzate = ["luca.riccio@gmail.com", "riccio.luca@gmail.com"]
 var autorizzata = false;
 
@@ -7,10 +7,28 @@ for (var i = 0; i<mailAutorizzate.length; i++){
   autorizzata = true
   }
 }
-
 if (autorizzata == true){
-  alert("Mail autorizzata");
+  alert("Mail autorizzata, buon divertimento!");
 }
 else {
-  alert("Mail non autorizzata");
+  alert("Mail non autorizzata, spiacenti non puoi giocare.");
 }
+
+
+var giocatore = (Math.floor(Math.random() * 6) + 1);
+var computer = Math.floor(Math.random() * 6) + 1;
+var vincitore;
+
+if (giocatore > computer) {
+  var vincitore = ("Vince il giocatore!")
+}
+else {
+  var vincitore = ("Vince il computer!")
+}
+console.log(vincitore);
+console.log(giocatore);
+console.log(computer);
+
+document.getElementById('player').innerHTML = "Dado del giocatore: " + giocatore;
+document.getElementById('cpu').innerHTML = "Dado del computer: " + computer;
+document.getElementById('win').innerHTML = vincitore;
